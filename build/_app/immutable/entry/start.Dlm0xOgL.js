676583ca24709187fbae1e4c8fd3a5b61eff52a1
LeaPhant <mail@lea.moe>
2024-06-04 16:52:01 +0200

    Set base path for gh pages

---
+++ build/_app/immutable/entry/start.Dlm0xOgL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DBrNEfDn.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -6,6 +6,9 @@ const config = {
         adapter: adapter({
             fallback: 'index.html'
         }),
+        paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
     },
 };
 
