40f12842a1728231fd5b2a6b5da0c242473c9669
LeaPhant <mail@lea.moe>
2024-06-04 16:28:40 +0200

    Add basic user list when no username specified

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,9 +15,28 @@
         };
     });
 
-    $: username = $url.hash.substring(1).toLowerCase() || "respektive";
-    $: user = data.rankings.find((u) => u.username.toLowerCase() === username);
+    $: username = $url.hash.substring(1).toLowerCase();
+    $: user = username ? data.rankings.find((u) => u.username.toLowerCase() === username) : null;
     $: rankings = data.rankings;
 </script>
 
-<CardWrapper {user} {rankings} />
+<style>
+    td.score {
+        text-align: right;
+        font-family: monospace;
+    }
+</style>
+
+{#if user }
+    <CardWrapper {user} {rankings} />
+{:else}
+    <table>
+        {#each rankings as user, index}
+            <tr>
+                <td>#{index + 1}</td>
+                <td><a href="#{user.username}">{user.username}</a></td>
+                <td class="score">{user.gained_score.toLocaleString()}</td>
+            </tr>  
+        {/each}
+    </table>
+{/if}
