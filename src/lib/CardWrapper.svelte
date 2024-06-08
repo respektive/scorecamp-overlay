f59e707c9073587c0b111d649b53e1e5cb2f1138
respektive <respektivee@gmail.com>
2024-06-08 16:05:43 +0200

    adjust surrounding players layout

--- src/lib/CardWrapper.svelte
+++ src/lib/CardWrapper.svelte
@@ -7,13 +7,18 @@
 </script>
 
 <div class="container">
-    {#if user.rank > 1}
-        <ScoreDelta main_user={user} compare_user={rankings[user.rank - 2]} />
-    {/if}
     <PlayerCard {user} />
-    {#if rankings[user.rank]}
-        <ScoreDelta main_user={user} compare_user={rankings[user.rank]} />
-    {/if}
+    <div class="deltas">
+        {#if user.rank > 1}
+            <ScoreDelta main_user={user} compare_user={rankings[user.rank - 2]} />
+        {/if}
+        {#if user.rank > 1 && rankings[user.rank]}
+            <div id="spacer" />
+        {/if}
+        {#if rankings[user.rank]}
+            <ScoreDelta main_user={user} compare_user={rankings[user.rank]} />
+        {/if}
+    </div>
 </div>
 
 <style>
@@ -21,5 +26,22 @@
         width: 100%;
         max-width: 1200px;
         margin: 2vh;
+    }
+
+    .deltas {
+        display: flex;
+        margin: 1em;
+        justify-content: space-evenly;
+    }
+
+    #spacer {
+        width: 0.1em;
+        border: 0;
+        margin: 0;
+        background: linear-gradient(
+            rgba(128, 128, 128, 0),
+            rgba(128, 128, 128, 0.3),
+            rgba(128, 128, 128, 0)
+        );
     }
 </style>
