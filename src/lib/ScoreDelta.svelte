d259ab34a4e7342ccc3e0eda470e787c3a033e8c
respektive <respektivee@gmail.com>
2026-05-22 13:19:54 +0200

    fix missing # in delta rank

--- src/lib/ScoreDelta.svelte
+++ src/lib/ScoreDelta.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <div class="delta">
-    <p id="username">{compare_user.rank} - {compare_user.teamName}</p>
+    <p id="username">#{compare_user.rank} - {compare_user.teamName}</p>
     <p id="score">
         {compare_user.scoreGained.toLocaleString("en-US")}
         <span id={delta > 0 ? "positive" : "negative"}>{delta.toLocaleString("en-US")}</span>
